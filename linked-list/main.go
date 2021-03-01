41b9eced047b02127939ce76ea4bde90e2ae9f41
taekrigi <taekrigi@taekrigiui-MacBookPro.local>
2021-03-01 15:06:26 +0900

    added linkedList

---
+++ linked-list/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+)
+
+type node struct {
+	data int
+	next *node
+}
+
+type linkedList struct {
+	head *node
+	length int
+}
+
+func (l *linkedList) prepend(n *node) {
+	second := l.head
+	l.head = n
+	l.head.next = second
+	l.length++
+}
+
+func (l linkedList) printListData() {
+	toPrint := l.head
+	for l.length != 0 {
+		fmt.Printf("%d ", toPrint.data)
+		toPrint = toPrint.next
+		l.length--
+	}
+	fmt.Printf("\n")
+}
+
+func (l *linkedList) deleteWithValue(value int) {
+	if l.length == 0 {
+		return
+	}
+
+	if l.head.data == value {
+		l.head = l.head.next
+		l.length--
+		return
+	}
+
+	toDelete := l.head
+
+	for i := 1; i < l.length; i++ {
+		if toDelete.next.data == value {
+			toDelete.next = toDelete.next.next
+			l.length--
+			return
+		}
+		toDelete = toDelete.next
+	}
+}
+
+func main() {
+	myList := linkedList{}
+	node1 := &node{data: 48}
+	node2 := &node{data: 18}
+	node3 := &node{data: 16}
+	node4 := &node{data: 11}
+	node5 := &node{data: 17}
+	myList.prepend(node1)
+	myList.prepend(node2)
+	myList.prepend(node3)
+	myList.prepend(node4)
+	myList.prepend(node5)
+	myList.printListData()
+	myList.deleteWithValue(11)
+	myList.printListData()
+}
