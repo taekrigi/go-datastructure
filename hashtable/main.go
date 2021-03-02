bd8969a1bd454038c12b0307656aae7321f82287
taekrigi <taekrigi@gmail.com>
2021-03-02 17:52:45 +0900

    added hashtable

--- hashtable/main.go
+++ hashtable/main.go
@@ -19,7 +19,91 @@ type bucketNode struct {
 	next *bucketNode
 }
 
+func (h *HashTable) Insert(key string) {
+	index := hash(key)
+	h.array[index].insert(key)
+}
+
+func (h *HashTable) Search(key string) bool {
+	index := hash(key)
+	return h.array[index].search(key)
+}
+
+func (h *HashTable) Delete(key string) {
+	index := hash(key)
+	h.array[index].delete(key)
+}
+
+func (b *bucket) insert(k string) {
+	if !b.search(k) {
+		newNode := &bucketNode{key: k}
+		newNode.next = b.head
+		b.head = newNode
+		return
+	}
+
+	fmt.Println("k already exist!")	
+}
+
+func (b *bucket) search(k string) bool {
+	currentNode := b.head
+	for currentNode != nil {
+		if currentNode.key == k {
+			return true
+		}
+		currentNode = currentNode.next
+	}
+	return false
+}
+
+func (b *bucket) delete(k string) {
+	if b.head.key == k {
+		b.head = b.head.next
+		return
+	}
+
+	previousNode := b.head
+	for previousNode.next != nil {
+		if previousNode.next.key == k {
+			previousNode.next = previousNode.next.next
+		}
+		previousNode = previousNode.next
+	}
+}
+
+func hash(key string) int {
+	sum := 0
+	for _, v := range key {
+		sum += int(v)
+	}
+	return sum % ArraySize
+}
+
+func Init() *HashTable {
+	result := &HashTable{}
+	for i := range result.array {
+		result.array[i] = &bucket{}
+	}
+	return result
+}
+
 func main() {
-	testHashTable := &HashTable{}
-	fmt.Println(testHashTable)
+	hashTable := Init()
+	list := []string {
+		"ERIC",
+		"KENNY",
+		"KYLE",
+		"STAN",
+		"RANDY",
+		"BUTTERS",
+		"TOKEN",
+	}
+
+	for _, v := range list {
+		hashTable.Insert(v)
+	}
+	
+	fmt.Println(hashTable.Search("STAN"))
+	hashTable.Delete("STAN")
+	fmt.Println(hashTable.Search("STAN"))
 }
